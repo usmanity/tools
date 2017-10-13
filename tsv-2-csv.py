eb35d12572c06b506c0a8fb4c8893736378516ef
Muhammad Usman <muhammad@usmanity.com>
2017-10-13 09:17:49 -0700

    update with some usage instructions for this script

--- tsv-2-csv.py
+++ tsv-2-csv.py
@@ -1,6 +1,18 @@
 import csv
 import sys
 
+if len(sys.argv) < 2:
+    print("""
+Usage:
+    python tsv-2-csv.py <tabbed-file> <output-csv-file>
+        
+Currently, both files are required.
+
+Credit: I learned about this from this stackoverflow answer: https://stackoverflow.com/a/5591258/318822
+    """)
+
+exit()
+
 tabbed_file = sys.argv[1]
 csv_file = sys.argv[2]
 
@@ -11,7 +23,3 @@ with open(tabbed_file, 'rb') as input_file:
 with open(csv_file, 'wb') as output_file:
     file_write = csv.writer(output_file, quotechar='', quoting=csv.QUOTE_NONE)
     file_write.writerows(contents)
-
-'''
-Note, I learned about this from this stackoverflow answer: https://stackoverflow.com/a/5591258/318822
-'''
