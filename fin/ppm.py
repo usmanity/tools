5753f9e4a9d44d1a798e97a73d157e8894f7c692
Muhammad Usman <muhammad@usmanity.com>
2018-04-21 14:27:57 -0700

    easy to calculate multiples now

--- fin/ppm.py
+++ fin/ppm.py
@@ -1,6 +1,8 @@
-stock_price = float(raw_input('Stock price: '))
-dividend_per_month = float(raw_input('Dividend per month: '))
+while(True):
+        stock_price = float(raw_input('Stock price: '))
+        dividend_per_month = float(raw_input('Dividend per month, in cents: '))
 
-ppm = dividend_per_month / stock_price * 100
+        ppm = dividend_per_month / stock_price
 
-print("%.2f" % ppm)
+        print("%.2f" % ppm)
+
