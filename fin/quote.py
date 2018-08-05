904fe3d2797f8be9752ce7f2e9c44208b3d3271d
Muhammad Usman <muhammad@usmanity.com>
2018-08-05 14:12:55 -0700

    adding some changes to quote.py, I'm thinking of consuming it from a different file and using each part of the pipeline as a way to figure out different information about a stock, adding cached folder to gitignore

--- fin/quote.py
+++ fin/quote.py
@@ -5,6 +5,21 @@ data = sys.stdin.readline()
 
 jsonData = json.loads(data)
 
-print(jsonData['QuickQuoteResult']['QuickQuote']['FundamentalData']);
+class Quote():
+    def __init__(self, quoteData):
+        self.quoteData = quoteData
+        self.fundamentalData = quoteData['QuickQuoteResult']['QuickQuote']['FundamentalData']
+        self.setupData(quoteData)
 
-# print(json.dumps(jsonData['QuickQuoteResult'], indent=4, sort_keys=True))
+    def getRawData(self):
+        return json.dumps(self.quoteData['QuickQuoteResult'], indent=4, sort_keys=True)
+
+    def getStats(self):
+        return json.dumps(self.fundamentalData)
+
+    def setupData(self, data):
+        self.usefulData = {}
+        print(data);
+
+q = Quote(jsonData)
+# print(q.getStats())
