1e0bec4dfcf0e0f51f38a40699878a4a3f4f157d
Muhammad Usman <muhammad@usmanity.com>
2018-08-07 10:36:35 -0700

    saving WIP for getting quote

--- fin/quote.py
+++ fin/quote.py
@@ -3,23 +3,28 @@ import json
 
 data = sys.stdin.readline()
 
-jsonData = json.loads(data)
+if len(data) > 1:
+    jsonData = json.loads(data)
+else:
+    with open('cached/orc_quote.txt', 'r') as file:
+        data = file.readlines()
+        jsonData = json.loads(data[0])
 
 class Quote():
     def __init__(self, quoteData):
         self.quoteData = quoteData
         self.fundamentalData = quoteData['QuickQuoteResult']['QuickQuote']['FundamentalData']
-        self.setupData(quoteData)
+        self.setupData(self.fundamentalData)
 
     def getRawData(self):
         return json.dumps(self.quoteData['QuickQuoteResult'], indent=4, sort_keys=True)
 
     def getStats(self):
-        return json.dumps(self.fundamentalData)
+        return json.dumps(self.usefulData)
 
     def setupData(self, data):
         self.usefulData = {}
-        print(data);
+        self.usefulData['price'] = round(float(data['MPreviousClose']), 2)
 
 q = Quote(jsonData)
-# print(q.getStats())
+print(q.getStats())
