d5a28efd300f85aecc53a6d35e253e148da7a7c9
Muhammad Usman <muhammad@squareup.com>
2019-02-03 08:10:22 -0800

    add rename script

--- rename.py
+++ rename.py
@@ -1,10 +1,16 @@
-import sys
+#!/usr/local/bin/python
+import os
+import argparse
 
-print("Arguments", str(sys.argv))
+# set up argument parser
+parser = argparse.ArgumentParser(description="Rename a file by replacing specific characters, e.g. underscores with dashes")
+parser.add_argument("file", help="File you want to rename")
+parser.add_argument("needle", help="Character you want replaced")
+parser.add_argument("replacement", help="The replacement character")
+args = parser.parse_args()
 
-print("""
-  TODO:
-  - Figure out how to accept arguments
-  - Ask before renaming files
-  - Get list of all files
-""")
+# new name by simply replacing each given input
+new_name = args.file.replace(args.needle, args.replacement)
+
+# use os.rename (comes with python) to rename
+os.rename(args.file, new_name)
